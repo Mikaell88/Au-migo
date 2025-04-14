3c668fa9e8f868585358a6fb0d72149b9de98fe6
Mikaell88 <mikaelsby@outlook.com.br>
2025-04-14 19:34:19 -0300

    Alteração no Nav e no CSS da Navegação

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import './cadastro.module.css';
+import styles from './App.module.css'; // CSS Module corretamente importado
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
 import Cadastro from './cadastro'; 
 import Login from './login';
-import MainPage from './mainPage'; // Importando a página principal
+import MainPage from './mainPage';
 import Altera from './altera';
 import Iniciopetwalker from './iniciopetwalker';
 import Iniciodonopet from './iniciodonopet';
 
-
-
-
-// Definindo o componente Navbar dentro do App.js
 function Navbar() {
   return (
-    <nav className="navbar">
-      <button className="nav-button">
+    <nav className={styles.navbar}>
+      <button className={styles['nav-button']}>
         <Link to="/main">Página Principal</Link>
       </button>
-      <button className="nav-button">
-        <Link to="/Login">Conecte-se</Link>
+      <button className={styles['nav-button']}>
+        <Link to="/login">Conecte-se</Link>
       </button>
-      <button className="nav-button">
-        <Link to="/Cadastro">Cadastro</Link>
+      <button className={styles['nav-button']}>
+        <Link to="/cadastro">Cadastro</Link>
       </button>
-      <button className="nav-button">
-        <Link to="/Altera">Alterar e Excluir</Link>
+      <button className={styles['nav-button']}>
+        <Link to="/altera">Alterar e Excluir</Link>
       </button>
-      <button className="nav-button">
-        <Link to="/Iniciopetwalker">Inicio PetWalker</Link>
+      <button className={styles['nav-button']}>
+        <Link to="/iniciopetwalker">Inicio PetWalker</Link>
       </button>
-      <button className="nav-button">
-        <Link to="/Iniciodonopet">Inicio Tutor</Link>
+      <button className={styles['nav-button']}>
+        <Link to="/iniciodonopet">Inicio Tutor</Link>
       </button>
-      
     </nav>
   );
 }
@@ -41,25 +37,28 @@ function Navbar() {
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="header">
+      <div className={styles.app}>
+        <header className={styles.header}>
           <h1>Au-migo</h1>
           <p>Conectando você a passeadores de cães confiáveis.</p>
-          <img src={`${process.env.PUBLIC_URL}/Pet1.png`} alt="Descrição da Imagem" className="main-image" />
+          <img
+            src={`${process.env.PUBLIC_URL}/Pet1.png`}
+            alt="Descrição da Imagem"
+            className={styles['main-image']}
+          />
         </header>
 
-        {/* Adicionando o Navbar aqui */}
         <Navbar />
 
-        <main className="main-content">
+        <main className={styles['main-content']}>
           <Routes>
-            <Route path="/" element={<MainPage />} /> {/* Usando MainPage em vez de HomePage */}
+            <Route path="/" element={<MainPage />} />
+            <Route path="/main" element={<MainPage />} />
             <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/login" element={<Login />} />
             <Route path="/altera" element={<Altera />} />
-            <Route path="/main" element={<MainPage />} /> {/* Usando MainPage também para /main */}
-            <Route path="/iniciopetwalker" element={<Iniciopetwalker />} /> {}
-            <Route path="/iniciodonopet" element={<Iniciodonopet />} /> {}
+            <Route path="/iniciopetwalker" element={<Iniciopetwalker />} />
+            <Route path="/iniciodonopet" element={<Iniciodonopet />} />
           </Routes>
         </main>
       </div>
